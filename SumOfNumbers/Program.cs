be82fe2e6a199477941661ea7d615d895cce1fa4
Stepanov Vadim <vadgat777@mail.ru>
2022-07-01 14:05:21 +0700

    first commit

---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int sum = 0;
+            int three = 3;
+            int five = 5;
+
+            int number = random.Next(0, 100);
+
+            Console.WriteLine("\n Рандомное число number = " + number);
+
+            for (int i = 0; i <= number; i++)
+            {
+                if ((i / five == five) || (i / three == three))
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine("\n Cумма всех положительных чисел меньше number (включая число), которые кратные 3 или 5 - sum = " + sum);
+        }
+    }
+}
